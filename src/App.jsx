c31923bdcffc6672047b4c1d9fc04582a2d83348
8hatch8 <yutaka.pg@gmail.com>
2022-03-23 00:49:51 +0900

    fix: JSXにリネーム

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { createApi } from "unsplash-js";
+import { ImageList } from "./ImageList";
+
+const App = () => {
+  const api = createApi({
+    accessKey: process.env.REACT_APP_API_KEY,
+  });
+
+  const pressEnter = (e) => {
+    if (e.key === "Enter") {
+      api.search
+        .getPhotos({ query: e.target.value, orientation: "landscape" })
+        .then((result) => console.log({ result }))
+        .catch(() => {
+          console.log("something went wrong!");
+        });
+    }
+  };
+
+  return (
+    <>
+      <div className='Search_Form'>
+        <div style={{ fontWeight: "bold" }}>Image Search</div>
+        <input type='text' placeholder='search by enter key' onKeyDown={(e) => pressEnter(e)} />
+      </div>
+      <ImageList />
+    </>
+  );
+};
+
+export default App;
