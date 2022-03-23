5a190c6db9e13344ec81f9a6635630e9b627f6c7
8hatch8 <yutaka.pg@gmail.com>
2022-03-23 14:54:32 +0900

    add: 写真取得機能の実装

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { createApi } from "unsplash-js";
-import ImageList from "./ImageList";
+
+const api = createApi({
+  accessKey: process.env.REACT_APP_API_KEY,
+});
+
+const PhotoComp = ({ photo }) => {
+  const { urls } = photo;
+  return (
+    <>
+      <img className='img' src={urls.regular} style={{ width: "500px" }} />
+    </>
+  );
+};
+
+const Body = ({ data }) => {
+  if (data === null) {
+    return null;
+  } else if (data.errors) {
+    return (
+      <div>
+        <div>{data.errors[0]}</div>
+        <div>PS: Make sure to set your access token!</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className='feed'>
+        <ul className='columnUl'>
+          {data.response.results.map((photo) => (
+            <li key={photo.id} className='li' style={{ listStyle: "none" }}>
+              <PhotoComp photo={photo} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
 
 const App = () => {
-  const api = createApi({
-    accessKey: process.env.REACT_APP_API_KEY,
-  });
+  const [response, setResponse] = useState(null);
 
   const pressEnter = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && e.target.value) {
       api.search
         .getPhotos({ query: e.target.value, orientation: "landscape" })
-        .then((result) => console.log({ result }))
+        .then((result) => setResponse(result))
         .catch(() => {
           console.log("something went wrong!");
         });
@@ -24,7 +59,7 @@ const App = () => {
         <div style={{ fontWeight: "bold" }}>Image Search</div>
         <input type='text' placeholder='search by enter key' onKeyDown={(e) => pressEnter(e)} />
       </div>
-      <ImageList props={null} />
+      <Body data={response} />
     </>
   );
 };
